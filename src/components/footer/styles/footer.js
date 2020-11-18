009d8ba18c6bee427801363aef2ae2ba9f3ccade
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-11-18 16:26:26 +0700

    Style footer Row

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -17,3 +17,13 @@ export const Column = styled.div`
   flex-direction: column;
   text-align: left;
 `;
+
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-gap: 15px;
+
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  }
+`;
