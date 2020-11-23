d1c7c8a21bb337428baba6a53429067d31becaad
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-11-23 14:16:47 +0700

    Update background ulr and delete Frame style

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -4,12 +4,10 @@ import { Link as ReactRouterLink } from 'react-router-dom';
 export const Background = styled.div`
   display: flex;
   flex-direction: column;
-  background: url(${({ src }) =>
-      src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg'})
+  background: url((${({ src }) =>
+      src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg'}))
     top left / cover no-repeat;
 `;
-
-export const Frame = styled.div``;
 
 export const Container = styled.div`
   display: flex;
