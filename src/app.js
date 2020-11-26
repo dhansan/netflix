48e0bb8f550a2cec5cb51893644913943e8de2a8
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-11-26 23:21:12 +0700

    import IsUserRedirect and Configuration Route signin

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, Browse, Signin, Signup } from './pages';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect } from './helpers/routes';
 
 export default function App() {
+  const user = {};
   return (
     <Router>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
       <Route exact path="/signin">
-        <Signin />
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+        >
+          <Signin />
+        </IsUserRedirect>
       </Route>
+
       <Route exact path="/signup">
         <Signup />
       </Route>
