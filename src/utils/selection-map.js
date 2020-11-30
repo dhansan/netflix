1381b7d287de6d23598900ebc567b456cfe9ec4e
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-11-30 22:24:26 +0700

    selectionMap genre series

--- src/utils/selection-map.js
+++ src/utils/selection-map.js
@@ -5,6 +5,22 @@ export default function selectionMap({ series, films }) {
         title: 'Documantaries',
         data: series.filter((item) => item.genre === 'documentaries'),
       },
+      {
+        title: 'Comedies',
+        data: series.filter((item) => item.genre === 'comedies'),
+      },
+      {
+        title: 'Children',
+        data: series.filter((item) => item.genre === 'children'),
+      },
+      {
+        title: 'Crime',
+        data: series.filter((item) => item.genre === 'crime'),
+      },
+      {
+        title: 'Feel Good',
+        data: series.filter((item) => item.genre === 'feel-good'),
+      },
     ],
   };
 }
