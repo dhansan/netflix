bb1e114998ed18db14da480e3476d0af060b70e6
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-12-05 22:43:53 +0700

    Configuration SelectProfileContainer

--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -11,6 +11,16 @@ export function SelectProfileContainer({ user, setProfile }) {
           <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
         </Header.Frame>
       </Header>
+
+      <Profiles>
+        <Profiles.Title>Who's watching?</Profiles.Title>
+        <Profiles.List>
+          <Profiles.User>
+            <Profiles.Picture src={user.photoURL} />
+            <Profiles.Name>{user.displayName}</Profiles.Name>
+          </Profiles.User>
+        </Profiles.List>
+      </Profiles>
     </>
   );
 }
